910061866d669052ab278b941980ae1ff3637137
lifutian <lifutian>
2019-11-25 17:36:09 +0800

    注释掉多余代码

--- boot-hello/src/main/java/com/example/boothello/enableTest/EnableTestBootStrap.java
+++ boot-hello/src/main/java/com/example/boothello/enableTest/EnableTestBootStrap.java
@@ -1,22 +1,22 @@
-package com.example.boothello.enableTest;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 启动类
- * @author Ryze
- * @date 2019-10-28 18:10
- */
-@Configuration
-@EnableHelloWorld
-public class EnableTestBootStrap {
-    public static void main(String[] args) {
-        //注册当前引导类
-        AnnotationConfigApplicationContext bootStrap = new AnnotationConfigApplicationContext(EnableTestBootStrap.class);
-        //获取 名为helloWorld 的bean
-        String helloWorld = bootStrap.getBean("helloWorld", String.class);
-        System.out.println(helloWorld);
-    }
-
-}
+//package com.example.boothello.enableTest;
+//
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 启动类
+// * @author Ryze
+// * @date 2019-10-28 18:10
+// */
+//@Configuration
+//@EnableHelloWorld
+//public class EnableTestBootStrap {
+//    public static void main(String[] args) {
+//        //注册当前引导类
+//        AnnotationConfigApplicationContext bootStrap = new AnnotationConfigApplicationContext(EnableTestBootStrap.class);
+//        //获取 名为helloWorld 的bean
+//        String helloWorld = bootStrap.getBean("helloWorld", String.class);
+//        System.out.println(helloWorld);
+//    }
+//
+//}
--- boot-hello/src/main/java/com/example/boothello/enableTest/HelloWorld.java
+++ boot-hello/src/main/java/com/example/boothello/enableTest/HelloWorld.java
@@ -1,17 +1,17 @@
-package com.example.boothello.enableTest;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 导入的类
- * @author Ryze
- * @date 2019-10-29 11:28
- */
-@Configuration
-public class HelloWorld {
-    @Bean
-    public String helloWorld() {
-        return "hello world";
-    }
-}
+//package com.example.boothello.enableTest;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 导入的类
+// * @author Ryze
+// * @date 2019-10-29 11:28
+// */
+//@Configuration
+//public class HelloWorld {
+//    @Bean
+//    public String helloWorld() {
+//        return "hello world";
+//    }
+//}
